71987797cf4041efbc8fc4e5382a226c9ceaba7b
Jay Sokolowski <jaysoko@gmail.com>
2019-03-24 22:43:12 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,11 +2,12 @@ class Cat
 require 'pry'
 attr_reader :name
 
-def initialize(name)
+def initialize(name, mood="nervous")
 @name = name
+@mood = mood
 end
 
-def mood(mood)
+def mood
  @mood = mood
 end
 
